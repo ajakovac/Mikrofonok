a3793e93311fd01ead612905efc79b120c46abce
ajakovac <antal.jakovac@gmail.com>
2025-05-19 08:48:22 +0200

    analysis cont'd, grinder sound

--- spectrogram.py
+++ spectrogram.py
@@ -17,14 +17,14 @@ class Spectogram:
         self.normalize_spectrum = kwargs.get("normalize_spectrum", True) # normalize the spectrum
         self.noice_reduction_constant = kwargs.get("noice_reduction_constant", 0.1) # constant for the noise reduction
 
-        self.Nnus = int(np.log2(self.numax/self.numin))*self.Noctave # number of frequency values we consider
-        self.frequencies = np.array([ 2**(k/self.Noctave)*self.numin for k in  range(self.Nnus)])
+        self.Nnus = int(self.nu_to_index(self.numax)/self.Noctave)*self.Noctave # number of frequency values we consider
+        self.frequencies = np.array([ self.index_to_nu(k) for k in  range(self.Nnus)])
         self.Nint = int(self.tint*self.sample_rate)
         self.K = np.empty((self.Nnus,self.Nint))
         self.L = np.empty((self.Nnus,self.Nint))
         self.spectrum_data = None
         self.data = None
-        self.maxtime = None
+        self.duration = None
 
         # initialize the psectral matrices
         rfactor = 2**(1/self.Noctave)-1
@@ -34,120 +34,134 @@ class Spectogram:
             self.K[i] = np.array([ f[n]*np.sin(2*np.pi*oo*n*self.sampling_dt)*oo for n in range(self.Nint) ])
             self.L[i] = np.array([ f[n]*np.cos(2*np.pi*oo*n*self.sampling_dt)*oo for n in range(self.Nint) ])
 
-    def set(self, data):
-        self.spectrum_data = None
-        self.data = data
-        self.maxtime = len(data)/self.sample_rate  # maximal time of the signal
-        return self
+    def data_slice(self, t1,t2, data=None):
+        if data is None:
+            data =self.data
+        n1 = int(t1*self.sample_rate)
+        n2 = int(t2*self.sample_rate)
+        return data[n1:n2].copy()
 
-    def calculate_spectrum(self, t=0):
-        if self.data is None:
-            print('data is not set')
-            return None
-        if t<0 or t+self.tint > self.maxtime:
-            print(f'time {t} is out of range: it must be in [0,{self.maxtime}]')
-            return None
-        n = int(t*self.sample_rate)
+    def nu_to_index(self, nu) -> float:
+        return np.log2(nu/self.numin)*self.Noctave
+
+    def index_to_nu(self, index) -> float:
+        return 2**(index/self.Noctave)*self.numin
+
+    def time_to_index(self, rate, *t):
+        indices = []
+        for ti in t:
+            if ti is None:
+                indices.append(int(self.duration*rate))
+            elif ti>self.duration:
+                print(f'time {ti} is out of range: it must be in [0,{self.duration}]')
+                indices.append(int(self.duration*rate))
+            elif ti< 0:
+                print(f'time {ti} is out of range: it must be in [0,{self.duration}]')
+                indices.append(0)
+            else:
+                indices.append(int(ti*rate))
+        if len(indices) == 1:
+            return indices[0]
+        else:
+            return tuple(indices)
+
+    def calculate_spectrum(self, n):
         my_spectrum = (self.K@self.data[n:n+self.Nint]/self.Nint)**2 + (self.L@self.data[n:n+self.Nint]/self.Nint)**2
         if self.normalize_spectrum:
             norm = self.noice_reduction_constant + np.linalg.norm(my_spectrum, ord=2)
             my_spectrum = my_spectrum/norm
         return my_spectrum
 
-    def __call__(self, t=0, t_end=None):
-        if self.data is None:
-            print('data is not set')
-            return None
-        if t_end is None:
-            t_end = self.maxtime - self.tint
+    def set(self, data):
+        self.spectrum_data = None
+        self.data = data
+        self.duration = len(data)/self.sample_rate  # maximal time of the signal
         self.spectrum_data = []
-        self.time_range = np.arange(t, t_end, self.timestep)
+        self.time_range = np.arange(0,self.duration - self.tint, self.timestep)
         for t in self.time_range:
-            self.spectrum_data.append(self.calculate_spectrum(t))
+            n = self.time_to_index(self.sample_rate, t)
+            self.spectrum_data.append(self.calculate_spectrum(n))
         self.spectrum_data = np.array(self.spectrum_data)
-        return self.time_range, self.spectrum_data
+        return self
 
-    def convert_range(self, t1, t2):
-        n1 = int(t1*self.sample_rate)
-        if t2 is not None:
-            n2 = int(t2*self.sample_rate)
-        else:
-            n2 = len(self.data)
-        return n1,n2
-
-    def play(self, t1 = 0, t2=None):
-        n1,n2 = self.convert_range(t1,t2)
-        channel_0_int16 = np.int16(self.data[n1:n2] / np.max(np.abs(self.data)) * 32767)
+    def play(self):
+        channel_0_int16 = np.int16(self.data / np.max(np.abs(self.data)) * 32767)
         sd.play(channel_0_int16, samplerate=self.sample_rate)
         sd.wait()
 
-    def plot_data(self, t1=0, t2=None, ax = None, **kwargs):
+    def plot_data(self, ax = None, **kwargs):
         if self.data is None:
             print('data is not set')
             return None
         if ax is None:
             fig, ax = plt.subplots()
-        n1,n2 = self.convert_range(t1,t2)
-        subt = np.arange(len(self.data[n1:n2]))/self.sample_rate + t1
-        ax.plot(subt, self.data[n1:n2], **kwargs)
+        subt = np.arange(len(self.data))/self.sample_rate
+        ax.plot(subt, self.data, **kwargs)
         return ax
     
     def plot_spectrum(self, t, ax = None, **kwargs):
         if self.spectrum_data is None:
             print('spectrum is not calculated')
             return None
-        if t<0 or t>self.maxtime:
-            print(f'time {t} is out of range: it must be in [0,{self.maxtime}]')
-            return None
         if ax is None:
             fig, ax = plt.subplots()
-        spectrum = self.spectrum_data[int(t/self.timestep)]
+        spectrum = self.spectrum_data[self.time_to_index(1/self.timestep,t)]
         ax.plot(self.frequencies, spectrum, **kwargs)
         ax.set_xscale('log')
         return ax
 
-    def max_freq(self, t, map = (lambda x: np.log(x), lambda x:np.exp(x))):
+    def find_maxima_in_spectrum_by_index(self, n, nmax=10, threshold=0):
         if self.spectrum_data is None:
             print('spectrum is not calculated')
             return None
-        if t<0 or t>self.maxtime:
-            print(f'time {t} is out of range: it must be in [0,{self.maxtime}]')
+        data = self.spectrum_data[n]
+        peak = []
+        for index in range(1,len(data)-1):
+            if data[index]>data[index-1] and data[index]>data[index+1]:
+                omega_list = np.arange(index-1,index+2)
+                Vlist = data[index-1:index+2]
+                T = np.array([omega_list**2, omega_list, np.ones(omega_list.shape)]).T
+                coeff = np.linalg.inv(T)@Vlist
+                max_position = -coeff[1]/(2*coeff[0])
+                max_value = np.array([max_position**2, max_position, 1]) @ coeff
+                if max_value >threshold:
+                    peak.append([max_position, self.index_to_nu(max_position), max_value])
+        return np.array(sorted(peak, key=lambda x: x[2])[::-1])[:nmax]
+
+    def find_maxima_in_spectrum(self, t, nmax=10, threshold=0):
+        return self.find_maxima_in_spectrum_by_index(self.time_to_index(1/self.timestep,t),nmax, threshold)
+
+    def find_maxima_in_spectrogram(self, nmax=10, threshold=0):
+        if self.spectrum_data is None:
+            print('spectrum is not calculated')
             return None
-        spectrum = self.spectrum_data[int(t/self.timestep)]
-        index = np.argmax(spectrum)
-        if index == 0 or index ==len(spectrum)-1:
-            return index, spectrum[index]
-        omega_list = map[0](self.frequencies[index-1:index+2])
-        Vlist = spectrum[index-1:index+2]
-        T = np.array([omega_list**2, omega_list, np.ones(omega_list.shape)]).T
-        coeff = np.linalg.inv(T)@Vlist
-        return map[1](-coeff[1]/(2*coeff[0]))
+        peaks = []
+        times = []
+        for n in range(len(self.spectrum_data)):
+            peaks.append(self.find_maxima_in_spectrum_by_index(n, nmax, threshold))
+            times.append(n*self.timestep)
+        return times, peaks
 
     def plot_octaves_2D(self, t, ax = None, **kwargs):
         if self.spectrum_data is None:
             print('spectrum is not calculated')
             return None
-        if t<0 or t>self.maxtime:
-            print(f'time {t} is out of range: it must be in [0,{self.maxtime}]')
-            return None
         if ax is None:
             fig, ax = plt.subplots()
-        res= self.spectrum_data[int(t/self.timestep)].copy()
+        res= self.spectrum_data[self.time_to_index(1/self.timestep,t)].copy()
         res = res.reshape(-1,self.Noctave)
         x = np.arange(res.shape[1]+1)
         y = np.arange(res.shape[0]+1)    
         mesh = ax.pcolormesh(x, y, res, **kwargs)
         return mesh
     
-    def show_spectrogram(self, t1=0, t2=None, ax = None, **kwargs):
+    def show_spectrogram(self, ax = None, **kwargs):
         if self.spectrum_data is None:
             print('spectrum is not calculated')
             return None
-        if t2 is None:
-            t2 = self.maxtime
+        spectrum = self.spectrum_data
         if ax is None:
             fig, ax = plt.subplots()
-        spectrum = self.spectrum_data[int(t1/self.timestep):int(t2/self.timestep)]
         colors = ["white", "blue"]  # White for negative, black for positive
         cmap = mcolors.LinearSegmentedColormap.from_list("custom_cmap", colors)
         norm = mcolors.TwoSlopeNorm(vmin=0, vcenter=0.1*spectrum.max(), vmax=spectrum.max())
